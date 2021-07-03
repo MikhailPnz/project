e04ec370dd809ea198d9773b2f6b0da8485d7b5e
Burenkov Mikhail <burenkoff.mihail@yandex.ru>
2021-07-03 11:15:07 +0300

    Initial commit

---
+++ src/main/java/ru/burenkov/weatherBroker/WeatherBrokerApplication.java
@@ -0,0 +1,13 @@
+package ru.burenkov.weatherBroker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherBrokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherBrokerApplication.class, args);
+	}
+
+}
